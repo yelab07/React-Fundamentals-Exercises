2ae9a9a7f7bb31392865ba73254359ea71ef5dc6
yelab07 <79489573+yelab07@users.noreply.github.com>
2022-07-05 10:50:57 -0500

    updates

--- L2. Rendering UI with React/1. JavaScript Expressions with JSX/Starter/src/App.js
+++ L2. Rendering UI with React/1. JavaScript Expressions with JSX/Starter/src/App.js
@@ -13,7 +13,13 @@ const App = () => {
   const aboutReact = (facts) =>
     `Release date: ${facts.releaseDate}. Language: ${facts.releaseDate}. License: ${facts.license}.`;
 
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <img src={logo} alt="img" />
+      <h2>{"React"}</h2>
+      <h3>{aboutReact(facts)}</h3>
+    </div>
+  );
 };
 
 export default App;
